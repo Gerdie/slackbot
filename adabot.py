88051bf05a1c6e649d4dc6818c90d2ee137c2495
Maria <mariakmoy@gmail.com>
2017-03-25 23:24:41 +0000

    Adding command functionality

--- adabot.py
+++ adabot.py
@@ -1,26 +1,38 @@
 import os
-import time
 from slackclient import SlackClient
 
 BOT_ID = os.environ.get("BOT_ID")
+TEST_CHANNEL_ID = u'C4PPUR35J'
+STUDENT_ONLY_CHANNEL_ID = 'C2APE9BJ9'
 
 slack_client = SlackClient(os.environ.get('SLACK_BOT_TOKEN'))
 
 
-def post_dinner_poll():
+def post_dinner_msg(channel_id):
     slack_client.api_call(
         "chat.postMessage",
-        channel="bottest",
+        channel=channel_id,
         as_user=True,
-        text='/poll "Are you coming to dinner tonight at 6pm?" "Yes!" No"'
+        text='Reminder: Dinner tonight at 6pm!'
+        )
+
+
+# TODO: Re-generate Auth token
+def post_dinner_poll(channel_id):
+    slack_client.api_call(
+        "chat.command",
+        token=os.environ.get('SLACK_BOT_TOKEN'),
+        channel=channel_id,
+        command='/poll',
+        text='"Are you coming to dinner tonight at 6pm?" "Yes!" No"'
         )
 
 if __name__ == "__main__":
-    READ_WOBSOCKET_DELAY = 1
     if slack_client.rtm_connect():
         print "Adabot connected and running!"
-        post_dinner_poll()
-        while True:
-            time.sleep(READ_WOBSOCKET_DELAY)
+        post_dinner_msg(TEST_CHANNEL_ID)
+        # print slack_client.api_call("channels.list")
+        post_dinner_poll(TEST_CHANNEL_ID)
+        print "post poll"
     else:
         print "Connection failed. Invalid Slack token or Bot ID"
